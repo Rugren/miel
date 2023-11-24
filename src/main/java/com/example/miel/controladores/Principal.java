c4fd4e415ac9061dba5f5c4a97e7a0b0724fbdb8
[rugren] <[rubgarciaruiz@gmail.com]>
2023-11-24 01:09:01 +0100

    Corregido y comprobado mi código con la carpeta: miel (profesor) 22-11-23 (ya corregido en el mío) Posteriormente borraré esa carpeta, ya que el código bien de ahí está correctamente aplicado en el mío.

--- src/main/java/com/example/miel/controladores/Principal.java
+++ src/main/java/com/example/miel/controladores/Principal.java
@@ -1,29 +1,52 @@
 package com.example.miel.controladores;
 
 
+import com.example.miel.modelo.EntradaMiel;
+import com.example.miel.repositorios.RepositorioEntradas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Controller
 // hay que darle a importar @Controller
 
 public class Principal {
+    @Autowired
+    RepositorioEntradas repo;
+
     @GetMapping({"/", "/index", "/index.html"})
     public String paginaInicial(Model model){
+        EntradaMiel entradaMiel=new EntradaMiel();
+        entradaMiel.setFecha(LocalDate.now());
+        entradaMiel.setParaje("CienLagos");
+        entradaMiel.setFlor("Romero");
+        entradaMiel.setLote("123-abc");
+        repo.save(entradaMiel);
+
+        model.addAttribute("listaEntradas", repo.findAll());
+
+        return "index";
+    }
+
+
+    /**
+     * Así era de un ejemplo que hicimos
+     * public String paginaInicial(Model model){
         ArrayList<String> miArray=new ArrayList<>();
         miArray.add("Patata");
         miArray.add("Lechuga");
         miArray.add("Tomillo");
         model.addAttribute("lista", miArray);
 
-
-
         model.addAttribute("nombre", "Rubén");
         return "index";
     }
+     **/
+
 
     @GetMapping("/ubicacion")
     public String paginaUbicacion(Model model){
